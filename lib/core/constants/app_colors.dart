939ed5182cbaaede7151b5920c3dda208b0c5706
Danya <danya.fabres@gmail.com>
2026-04-25 01:32:10 -0400

    feat: sprint 0 verificación estructura base(sujeto a cambios )

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0D1B2A);
+  static const Color secondary = Color(0xFF1B263B);
+  static const Color accent = Color(0xFF00B4D8);
+  static const Color background = Color(0xFFF5F7FA);
+}
